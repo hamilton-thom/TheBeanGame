3429be6c91b443b8f80d64076c2128afc5fc74a9
hamilton-thom <35115145+hamilton-thom@users.noreply.github.com>
2018-07-14 12:13:06 +0100

    De-bug and fixing randomness

--- Main.hs
+++ Main.hs
@@ -3,6 +3,9 @@
 -- optimal strategies.
 
 import Data.List
+import Data.Maybe
+import Control.Monad (replicateM)
+import System.Random
 
 (|>) :: a -> (a -> b) -> b
 (|>) x f = f x
@@ -22,78 +25,104 @@ data Card
   | Blue
   | Wax
   | Coffee
+  deriving (Show, Read, Eq)
 
-beanCounts :: [Card Int]
-beanCounts = [Cocoa, 4; Garden, 6;, Red, 8;, BlackEyed, 10; Soy, 12;
-              Green, 14; StinkyG, 16; Chili, 18; Blue, 20; Wax, 22; Coffee, 24]
+beanCounts :: [(Card, Int)]
+beanCounts = [ (Cocoa, 4)
+             , (Garden, 6)
+             , (Red, 8)
+             , (BlackEyed, 10)
+             , (Soy, 12)
+             , (Green, 14)
+             , (StinkyG, 16)
+             , (Chili, 18)
+             , (Blue, 20)
+             , (Wax, 22)
+             , (Coffee, 24)
+             ]
 
-data Deck = [Card]
-data Hand = [Card Int]
+baseDeck = concat repeatedBeans
+  where repeatedBeans = beanCounts |> map (\(b, n) -> replicate n b)
+
+type Deck = [Card]
+type Hand = [(Card, Int)]
+type Score = Int
 data Field =
-  | Field2 (Maybe Card) (Maybe Card)
-  | Field3 (Maybe Card) (Maybe Card) (Maybe Card)
-data Player = Player Field Hand Int
+    Field2 (Maybe (Card, Int)) (Maybe (Card, Int))
+  | Field3 (Maybe (Card, Int)) (Maybe (Card, Int)) (Maybe (Card, Int))
+data Player = Player Field Hand Score
 
 -- During the game each player will do through three phases.
 -- In each phase they will always have some common options, these
 -- are encapsulated in the 'AlwaysOptions' type.
 data AlwaysOptions = Quit | FinishTurn | Harvest Card | BuyBeanField
-  deriving Show, Read
+  deriving (Show, Read, Eq)
 
 data PlantStatus = NothingPlanted | OnePlanted | TwoPlanted
-data TurnStatus = InitialPlant | Discard | MainTurn
+  deriving (Show, Read, Eq)
+
+data TurnStatus = InitialPlant | Discarding | MainTurn
+  deriving (Show, Read, Eq)
 
 data TurnStart = TS AlwaysOptions | TSPlant Card
-  deriving Show, Read
+  deriving (Show, Read, Eq)
 
 data TurnDiscard = TD AlwaysOptions | Discard Card
-  deriving Show, Read
+  deriving (Show, Read, Eq)
 
 data TurnMain = TM AlwaysOptions | TMPlant Card
-  deriving Show, Read
+  deriving (Show, Read, Eq)
 
 -- Helper function to determine if a player can plant a given card.
+-- Check if a.) there is a spare slot, and b.) in the case where there
+-- is no spare slot, whether or not the card you're trying to plant
+-- is the same as one that's already planted.
 canPlant :: Field -> Card -> Bool
 canPlant (Field2 mc1 mc2) c =
-  let maybeCheck = do c1 <- mc1
-                      c2 <- mc2
-                      return (c `elem` [c1; c2])
+  let maybeCheck = do (c1, _) <- mc1
+                      (c2, _) <- mc2
+                      return (c `elem` [c1, c2])
   in case maybeCheck of
-     | Maybe b -> b
-     | Nothing -> True
+     Just b -> b
+     Nothing -> True
 
-canPlant (Field2 mc1 mc2 mc3) c =
-  let maybeCheck = do c1 <- mc1
-                      c2 <- mc2
-                      c3 <- mc3
-                      return (c `elem` [c1; c2; c3])
+canPlant (Field3 mc1 mc2 mc3) c =
+  let maybeCheck = do (c1, _) <- mc1
+                      (c2, _) <- mc2
+                      (c3, _) <- mc3
+                      return (c `elem` [c1, c2, c3])
   in case maybeCheck of
-     | Maybe b -> b
-     | Nothing -> True
+     Just b -> b
+     Nothing -> True
 
 -- Helper function to determine if a player can harvest a given field.
+-- Remember that a field is a tuple of Maybe (Card, Int) values. So
+-- when checking if we can harvest something, we need to compare against
+-- only the Card part of the Maybe, and ignore the Int part.
 canHarvest :: Field -> Card -> Bool
-canHarvest (Field mc1 mc2) c =
-  cardList = [mc1; mc2] |> filter (isJust)
+canHarvest (Field2 mc1 mc2) c =
   case cardList of
-    | [] -> False
-    | cs -> c `elem` cs
+    [] -> False
+    cs -> (Just c) `elem` cs
+  where
+    cardList = [mc1, mc2] |> filter (isJust) |> map (fmap fst)
 
-canHarvest (Field mc1 mc2 mc3) c =
-  cardList = [mc1; mc2; mc3] |> filter (isJust)
+canHarvest (Field3 mc1 mc2 mc3) c =
   case cardList of
-    | [] -> False
-    | cs -> c `elem` cs
+    [] -> False
+    cs -> (Just c) `elem` cs
+  where
+    cardList = [mc1, mc2, mc3] |> filter (isJust) |> map (fmap fst)
 
 -- pc is the number of cards you've planted this turn.
 validStartMove :: Player -> TurnStart -> PlantStatus -> Bool
-validStart (Player field hand score) move plantStatus =
+validStartMove (Player field hand score) move plantStatus =
   case move of
-    | TS BuyBeanField -> score >= 3
-    | TS (Harvest c)  -> canHarvest field c
-    | TS FinishTurn   -> plantStatus /= NothingPlanted
-    | TS _            -> True
-    | TSPlant c       -> canPlant field c
+    TS BuyBeanField -> score >= 3
+    TS (Harvest c)  -> canHarvest field c
+    TS FinishTurn   -> plantStatus /= NothingPlanted
+    TS _            -> True
+    TSPlant c       -> canPlant field c
 
 validDiscardMove :: Player -> Bool
 validDiscardMove (Player field hand score) = length hand > 0
@@ -101,10 +130,10 @@ validDiscardMove (Player field hand score) = length hand > 0
 validMainMove :: Player -> TurnMain -> Bool
 validMainMove (Player field hand score) move =
   case move of
-    | TM BuyBeanField -> score >= 3
-    | TM (Harvest c)  -> canHarvest field c
-    | TM _            -> True
-    | TMPlant c       -> canPlant field c
+    TM BuyBeanField -> score >= 3
+    TM (Harvest c)  -> canHarvest field c
+    TM _            -> True
+    TMPlant c       -> canPlant field c
 
 -- Now we want to actually start the game proper.
 
@@ -128,25 +157,20 @@ validMainMove (Player field hand score) move =
 
 
 -- Way down here we start the actual making of the deck and playing of the game.
+nCards :: Int
+nCards = length baseDeck
 
-nCards = beanCounts |> map snd |> sum
+makeFloats :: Int -> Int -> [Double]
+makeFloats n seed = take n (randoms g)
+  where g = mkStdGen seed
 
-randomGen = mkStdGen 1
+makeOrderedRange:: Int -> Int -> [Int]
+makeOrderedRange n seed = map fst sortedFloats
+  where baseFloats = zip [1..n] (makeFloats n seed)
+        sortedFloats = baseFloats |> sortOn snd
 
-makeFloats :: Int -> [Double]
-makeFloats n = replicateM n (randomIO :: IO Double)
-
-makeOrderedRange:: Int -> [Int]
-makeOrderedRange n =
-  where baseFloats = makeFloats n
-        sortedFloats = zip [1..n] (sort baseFloats)
-
-
-
-
-makeDeck n =
-    let rGen = mkStdGen 1 in
-    where
-      ns = [1..n]
-      extract i ns = (ns !! (i-1), take (i-1) ns ++ drop i ns)
-      pickRand = map (\x -> (randomR (0, x-1) rGen)) (reverse [0..x-1])
+makeDeck :: [a] -> Int -> [a]
+makeDeck inputDeck seed = map snd orderedList
+  where ordering = makeOrderedRange (length inputDeck) seed
+        zippedList = zip ordering inputDeck
+        orderedList = sortOn fst zippedList
