908e7f0dd655e5fd49347647dccd78b51ec5d25a
hamilton-thom <35115145+hamilton-thom@users.noreply.github.com>
2018-08-02 20:05:55 +0100

    Improving Main.hs

--- Main.hs
+++ Main.hs
@@ -44,14 +44,6 @@ beanCounts = [ (Cocoa, 4)
 baseDeck = concat repeatedBeans
   where repeatedBeans = beanCounts |> map (\(b, n) -> replicate n b)
 
-type Deck = [Card]
-type Hand = [(Card, Int)]
-type Score = Int
-data Field =
-    Field2 (Maybe (Card, Int)) (Maybe (Card, Int))
-  | Field3 (Maybe (Card, Int)) (Maybe (Card, Int)) (Maybe (Card, Int))
-data Player = Player Field Hand Score
-
 -- During the game each player will do through three phases.
 -- In each phase they will always have some common options, these
 -- are encapsulated in the 'AlwaysOptions' type.
@@ -73,69 +65,164 @@ data TurnDiscard = TD AlwaysOptions | Discard Card
 data TurnMain = TM AlwaysOptions | TMPlant Card
   deriving (Show, Read, Eq)
 
+-- Each player has certain attributes that need to be kept track of
+-- during the game.
+type Score = Int
+newtype Hand = Han [Card]
+
+newtype Field = Field [Maybe (Card, Int)]
+maxFields = 3
+
+data Player = Player Field Hand Score PlantStatus
+
+-- The remaining cards in the deck.
+newtype Deck = Dec [Card]
+-- The cards in the Middle.
+newtype Middle = Mid [(Card, Int)]
+-- The discard pile.
+newtype Discard = Dis [Card]
+
+data GlobalState = Global Deck Middle Discard [Player]
+
 -- Helper function to determine if a player can plant a given card.
 -- Check if a.) there is a spare slot, and b.) in the case where there
 -- is no spare slot, whether or not the card you're trying to plant
 -- is the same as one that's already planted.
 canPlant :: Field -> Card -> Bool
-canPlant (Field2 mc1 mc2) c =
-  let maybeCheck = do (c1, _) <- mc1
-                      (c2, _) <- mc2
-                      return (c `elem` [c1, c2])
-  in case maybeCheck of
-     Just b -> b
-     Nothing -> True
-
-canPlant (Field3 mc1 mc2 mc3) c =
-  let maybeCheck = do (c1, _) <- mc1
-                      (c2, _) <- mc2
-                      (c3, _) <- mc3
-                      return (c `elem` [c1, c2, c3])
-  in case maybeCheck of
-     Just b -> b
-     Nothing -> True
+canPlant (Field []) _     = False
+canPlant (Field (x:xs)) c =
+  case x of
+    Nothing      -> True
+    Just (c1, n) -> c1 == c || canPlant (Field xs) c
 
 -- Helper function to determine if a player can harvest a given field.
 -- Remember that a field is a tuple of Maybe (Card, Int) values. So
 -- when checking if we can harvest something, we need to compare against
 -- only the Card part of the Maybe, and ignore the Int part.
 canHarvest :: Field -> Card -> Bool
-canHarvest (Field2 mc1 mc2) c =
-  case cardList of
-    [] -> False
-    cs -> (Just c) `elem` cs
-  where
-    cardList = [mc1, mc2] |> filter (isJust) |> map (fmap fst)
-
-canHarvest (Field3 mc1 mc2 mc3) c =
-  case cardList of
-    [] -> False
-    cs -> (Just c) `elem` cs
-  where
-    cardList = [mc1, mc2, mc3] |> filter (isJust) |> map (fmap fst)
+canHarvest (Field []) _     = False
+canHarvest (Field (x:xs)) c =
+  case x of
+    Just (c1, n) | c1 == c -> True
+    Just (c2, n) | c2 /= c -> canHarvest (Field xs) c
+    Nothing                -> canHarvest (Field xs) c
 
 -- pc is the number of cards you've planted this turn.
-validStartMove :: Player -> TurnStart -> PlantStatus -> Bool
-validStartMove (Player field hand score) move plantStatus =
+validStartMove :: Player -> TurnStart -> Bool
+validStartMove (Player (Field xs) hand score plantStatus) move =
   case move of
-    TS BuyBeanField -> score >= 3
-    TS (Harvest c)  -> canHarvest field c
+    TS BuyBeanField -> score >= 3 && length xs < maxFields
+    TS (Harvest c)  -> canHarvest (Field xs) c
     TS FinishTurn   -> plantStatus /= NothingPlanted
     TS _            -> True
-    TSPlant c       -> canPlant field c
+    TSPlant c       -> canPlant (Field xs) c
 
 validDiscardMove :: Player -> Bool
-validDiscardMove (Player field hand score) = length hand > 0
+validDiscardMove (Player field (Han hand) score turnStatus) = length hand > 0
 
 validMainMove :: Player -> TurnMain -> Bool
-validMainMove (Player field hand score) move =
+validMainMove (Player (Field xs) hand score turnStatus) move =
   case move of
-    TM BuyBeanField -> score >= 3
-    TM (Harvest c)  -> canHarvest field c
+    TM BuyBeanField -> score >= 3 && length xs < maxFields
+    TM (Harvest c)  -> canHarvest (Field xs) c
     TM _            -> True
-    TMPlant c       -> canPlant field c
+    TMPlant c       -> canPlant (Field xs) c
 
--- Now we want to actually start the game proper.
+-- Now we want to start the game proper.
+-- Need to go through the mechanics of a turn.
+-- You're given a set of cards, and then have several options
+-- which will recursively run until you reach the end of your turn,
+-- all the while you're changing the "World" which is largely just your
+-- player result.
+
+type PlayerID = Int
+
+nextPlayer nPlayers playerID =
+  if playerID == nPlayers then
+    1
+  else
+    playerID + 1
+
+type AllPlayers = [Player]
+
+plant :: Card -> Field -> Field
+plant c (Field xs) =
+  if canPlant (Field xs) c == False
+    then Field xs
+    else Field (modified xs)
+  where modified ns =
+          case ns of
+            []                          -> []
+            (Just (c1, n)):cs | c1 == c -> (Just (c1, n + 1)):cs
+            c1:cs                       -> c1 : (modified cs)
+
+middleCards :: Middle -> [Card]
+middleCards middle =
+  case middle of
+    Mid []          -> []
+    Mid ((c, n):cs) -> c : (filter (/= c) (middleCards cs))
+
+addCard :: Middle -> Card -> Middle
+addCard middle c =
+  if length matchingCards = 0 then
+     (c, 1) : unmatchingCards
+     else matchingCards ++ unmatchingCards
+
+  where matchingCards = [(c', n+1) | (c', n) <- middle, c' == c]
+        unmatchingCards = [(c', n) | (c', n) <- middle, c' /= c]
+
+addCards :: Middle -> [Card] -> Middle
+addCards m []     = m
+addCards m (c:cs) = addCards (addCard m c) cs
+
+-- Middle :: Mid [(Card, Int)]
+-- This function needs improving so that we sort the discard pile.
+matchDraw :: Discard -> Middle -> (Discard, Middle)
+matchDraw discard middle =
+  case discard of
+    Dis [] -> (discard, middle)
+    Dis (c:cs) -> if c `elem` midCards then
+                    matchDraw (Dis cs) (addCard middle c)
+                    else (discard, middle)
+  where midCards = middleCards middle
+
+data GlobalState = Global Deck Middle Discard [Player]
+
+-- Draws n cards from the Deck, taking into account
+-- that the deck might be exhausted, then returns the
+-- resulting GlobalState and the cards which were drawn.
+-- These are then appended to the middle, with all the
+-- jiggery-pokery of the discard set-up done automatically.
+safeDraw :: GlobalState -> Int -> (GlobalState, [Card])
+safeDraw gs 0 = (gs, [])
+safeDraw Global
+          (Dec deck)
+          middle
+          (Dis discard)
+         n =
+  case deck of
+    [] -> safeDraw Global (Dec []) middle (Dis start)
+
+
+  where splitPos = length discard - 1
+        start, [end] = splitAt splotPos discard
+
+
+
+drawMain :: GlobalState -> GlobalState
+drawMain (Global deck middle discard _) =
+
+  safeDraw |> addCards |> matchDraw discard
+
+-- draw and then resolve the matching
+  where topThreeCards = safeDraw 3 deck
+
+  if deckLen >= n then
+    take n deck
+    else take deckLen deck ++ drop (discardLen - cardDifference) discard
+  where deckLen = length deck
+        discardLen = length discard
+        cardDifference = n - deckLen
 
 
 
@@ -148,6 +235,25 @@ validMainMove (Player field hand score) move =
 
 
 
+
+{-
+moveStart :: GlobalState ->   -- The global state of the game.
+             PlayerID ->      -- The ID of the player making the move.
+             TurnStart ->     -- The turn that they're trying to make.
+             GlobalState      -- The returned global state.
+moveStart gs pId ts =
+  if validStart player ts then
+     case ts of
+     | TSPlant c -> plant c
+
+  else gs
+
+
+  where
+    player = case gs of Global _ _ _ players -> players !! (pId - 1)
+    plantStatus = case player of Player _ _ _ ps -> ps
+
+-}
 
 
 
@@ -169,8 +275,8 @@ makeOrderedRange n seed = map fst sortedFloats
   where baseFloats = zip [1..n] (makeFloats n seed)
         sortedFloats = baseFloats |> sortOn snd
 
-makeDeck :: [a] -> Int -> [a]
-makeDeck inputDeck seed = map snd orderedList
+makeDeck :: Int -> [a] -> [a]
+makeDeck seed inputDeck = map snd orderedList
   where ordering = makeOrderedRange (length inputDeck) seed
         zippedList = zip ordering inputDeck
         orderedList = sortOn fst zippedList
